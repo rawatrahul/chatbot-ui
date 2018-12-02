b4f64960a6191ad711d05179a3bb00feff05cb53
Rahul Rawat <rahulrawat593@gmail.com>
2018-12-02 15:39:26 -0700

    Add ui code and icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
+import 'semantic-ui-css/semantic.min.css';
+import bot from './bot.png';
 import './App.css';
 
+const getAnswer = async (myBody, func) => {
+    const response = await fetch('http://127.0.0.1:5000/query', {
+        method: 'POST',
+        body: JSON.stringify(myBody),
+        headers:{
+            'Content-Type': 'text/plain'
+        }
+    });
+    const myJson = await response.json();
+    func(myJson['answer']);
+}
+
 class App extends Component {
+
+    componentDidMount() {
+        addResponseMessage("Hi, I am ITllect.");
+    }
+
+    handleNewUserMessage = (newMessage) => {
+        console.log(`New message incoming! ${newMessage}`);
+        getAnswer({"query":newMessage},addResponseMessage);
+    }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+		<Widget
+            title = {"Welcome Sundevil 🔱"}
+            subtitle = {"How can i help you ?"}
+            profileAvatar = {bot}
+		    handleNewUserMessage={this.handleNewUserMessage}
+		/>
       </div>
     );
   }
